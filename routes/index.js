b8f58bcdb583944e4c273db5f58bb603af39d6ea
ferdinand-dhbw <inf19211@lehre.dhbw-stuttgart.de>
2020-12-10 21:50:42 +0100

    fix red errors (eslint) (router)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+// eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+export default router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+// eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
+export default router;
