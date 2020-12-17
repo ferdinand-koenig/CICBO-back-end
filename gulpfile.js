d3b02d8da35a6c6ae5dd0d26e89bac3e3535625d
ferdinand-dhbw <inf19211@lehre.dhbw-stuttgart.de>
2020-12-17 17:48:54 +0100

    fix bugs (mocha in console)

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,22 @@ const eslint = require("gulp-eslint");
 const uglify = require("gulp-uglify");
 const typedoc = require("gulp-typedoc");
 const exec = require('child_process').exec;
+const execSync = require('child_process').execSync;
 const fs = require("fs");
 const validate = require('jsonschema').validate;
 
 function runTests(cb) {
-    exec('mocha -r ts-node/register ./tests/app.test.ts', function (err, stdout, stderr) {
+    exec('npx mocha -r ts-node/register ./tests/app.test.ts', function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
-        if(!err) console.log("\u001b[32m √\u001b[0m All tests passed");
+        if(!err) console.log("\u001b[32m [V] All tests passed\u001b[0m");
         cb(err);
     });
 }
 function compileTypeScript(cb) {
     exec('tsc --build ./tsconfig.json', function (err, stdout, stderr) {
         console.log(stderr);
-        if(!err) console.log("\u001b[32m √\u001b[0m TypeScript compiled");
+        if(!err) console.log("\u001b[32m [V] TypeScript compiled\u001b[0m");
         cb(err);
     });
 }
@@ -30,7 +31,7 @@ function runLinter(cb) {
         .pipe(eslint.failAfterError())
         .on('end', () => {
             console.log(" ");
-            console.log("\u001b[32m √\u001b[0m Found no errors or warnings:\u001b[32m passed\u001b[0m");
+            console.log("\u001b[32m [V] Found no errors or warnings: passed\u001b[0m");
             cb();
         });
 }
@@ -38,27 +39,23 @@ function minifyJS(cb) {
     src(['./app.js'])
         .pipe(uglify())
         .pipe(dest('./', {overwrite: true}))
-    console.log(" ");
-    console.log("\u001b[32m √\u001b[0m app.js minified")
+    console.log("\n\u001b[32m [V] app.js minified\u001b[0m")
     cb();
 }
 function checkForSecretsJSON(cb){
     if (fs.existsSync('./secrets/mongo-settings-with-credentials.json')) {
         if(validate(JSON.parse(fs.readFileSync("./secrets/mongo-settings-with-credentials.json")), secretSchema, {required: true}).valid){
-            console.log("\n\u001b[32m √\u001b[0m Secrets-file is valid")
+            console.log("\n\u001b[32m [V] Secrets-file is valid\u001b[0m")
             cb();
         }else
-            cb(new Error("\u001b[31m X\u001b[0m Secrets-file not valid (./secrets/mongo-settings-with-credentials.json)!"));
+            cb(new Error("\u001b[31m [X] Secrets-file not valid (./secrets/mongo-settings-with-credentials.json)!\u001b[0m"));
     }else
-        cb(new Error("\u001b[31m X\u001b[0m Secrets-file not found (./secrets/mongo-settings-with-credentials.json)!"));
+        cb(new Error("\u001b[31m [X] Secrets-file not found (./secrets/mongo-settings-with-credentials.json)!\u001b[0m"));
 }
-function start(cb){
-    console.log("\u001b[32m √\u001b[0m listening on port 3000");
-    exec('node ./bin/www', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
+function start(){
+    console.log("\u001b[32m [V] listening on port 3000 \u001b[0m");
+    console.log("\u001b[93m---server-output---\u001b[0m")
+    return execSync('node ./bin/www', {stdio: 'inherit'});
 }
 function createDocumentation() {
     return src(["./app.ts"])
@@ -78,7 +75,7 @@ function createDocumentation() {
             exclude: "**/node_modules/**",
             ignoreCompilerErrors: false,
             version: true,
-        }).on('end', ()=>{console.log("\n\u001b[32m √\u001b[0m Documentation created in './documentation/': open index.html in browser")}));
+        }).on('end', ()=>{console.log("\n\u001b[32m [V] Documentation created in './documentation/': open index.html in browser \u001b[0m")}));
 }
 
 const build = series(runLinter, compileTypeScript, minifyJS, checkForSecretsJSON, runTests);
