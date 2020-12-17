dd322feb308aaf899d35bcf844c2b89ed8ed6b93
ferdinand-dhbw <inf19211@lehre.dhbw-stuttgart.de>
2020-12-17 12:17:19 +0100

    add eslint and mocha to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,27 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
+const { src, dest } = require("gulp");
+const eslint = require("gulp-eslint");
+const exec = require('child_process').exec;
+
+function runTests(cb) {
+    exec('mocha -r ts-node/register ./tests/app.test.ts', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
 }
 
-exports.default = defaultTask
+function runLinter(cb) {
+    return src(['**/*.js', '**/*.ts', '*.js', '*.ts', '!node_modules/**', '!dist/**', '!bin/**', '!app.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+        .on('end', () => {
+            console.log("Found no errors or warnings:\u001b[32m passed\u001b[0m");
+            cb();
+        });
+}
+
+exports.lint = runLinter;
+exports.test = runTests;
+
+exports.default = runLinter
